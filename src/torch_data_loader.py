f28239ac2ccdf088c9d71fb6e9aee94326482f72
YangWu1227 <yangwu2020@gmail.com>
2023-08-06 07:17:07 -0500

    feat: add train, val, test split for data loader

--- src/torch_data_loader.py
+++ src/torch_data_loader.py
@@ -1,6 +1,7 @@
 import os
 import logging
 from typing import List, Dict, Tuple, Any, Union, Optional
+import shutil
 
 import numpy as np
 import pandas as pd 
@@ -13,6 +14,7 @@ from torch.utils.data import DataLoader, Dataset
 import torchvision.transforms as transforms
 
 from PIL import Image 
+from sklearn.model_selection import train_test_split
 
 # ----------------------- Download spacy english model ----------------------- #
 
@@ -138,7 +140,7 @@ class FlickrDataset(Dataset):
     This class implements the custom Flickr8k dataset, which represents a map from keys to 
     data samples.
     """
-    def __init__(self, image_dir: str, captions_file: str, transform: Optional[transforms.Compose] = None, freq_threshold: int = 5):
+    def __init__(self, image_dir: str, captions_file: str, transform: Optional[transforms.Compose] = None, freq_threshold: int = 5, test_mode: bool = False):
         """
         Constructor for the FlickrDataset class.
 
@@ -152,10 +154,12 @@ class FlickrDataset(Dataset):
             Transforms to apply on the images.
         freq_threshold : int, default=5
             The threshold for word frequency.
+        test_mode : bool, default=False
+            Whether to run in test mode, which samples only 64 examples.
         """
         self.image_dir = image_dir
-        # This reads in the txt file mapping image file names to captions
-        self.data = pd.read_csv(captions_file)
+        # This reads in the txt file mapping image file names to captions, and takes only the first 64 rows if in test mode
+        self.data = pd.read_csv(captions_file).iloc[:64] if test_mode else pd.read_csv(captions_file)
         self.transform = transform
         self.images = self.data['image']
         self.captions = self.data['caption']
@@ -250,6 +254,7 @@ def data_loader(
     num_workers: int = 8,
     shuffle: bool = True,
     pin_memory: bool = True,
+    test_mode: bool = False
 ) -> Tuple[DataLoader, FlickrDataset]:
     """
     Get the data loader.
@@ -270,13 +275,15 @@ def data_loader(
         Whether to shuffle the data.
     pin_memory : bool, default=True
         Whether to pin memory, which speeds up the transfer of data from CPU to GPU.
+    test_mode : bool, default=False
+        Whether to run in test mode, which samples only two batches worth of data.
 
     Returns
     -------
     Tuple[DataLoader, FlickrDataset]
         The data loader and the dataset.
     """
-    dataset = FlickrDataset(image_dir=image_dir, captions_file=captions_file, transform=transform)
+    dataset = FlickrDataset(image_dir=image_dir, captions_file=captions_file, transform=transform, test_mode=test_mode)
     pad_idx = dataset.vocab.token_to_index['<PAD>']
     data_loader = DataLoader(
         dataset=dataset,
@@ -288,6 +295,71 @@ def data_loader(
     )
 
     return data_loader, dataset
+
+# ------------------- Functions for train, val, test split ------------------- #
+
+def split_data(data: pd.DataFrame, val_ratio: float = 0.2) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    """
+    Split the data into train, validation, and test sets based on unique images.
+
+    Parameters
+    ----------
+    data : pd.DataFrame
+        The original data.
+    val_ratio : float
+        The ratio of the validation set.
+
+    Returns
+    -------
+    Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]
+        The train, validation, and test sets.
+    """
+    unique_images = data['image'].unique()
+    
+    # Split unique images into 90% train_temp and 10% test
+    train_temp_images, test_images = train_test_split(unique_images, test_size=0.1, random_state=42)
+    
+    # Split train_temp_images into 80% train and 20% val
+    train_images, val_images = train_test_split(train_temp_images, test_size=val_ratio, random_state=42)
+    
+    # Get the corresponding rows from the data DataFrame
+    train_data = data[data['image'].isin(train_images)]
+    val_data = data[data['image'].isin(val_images)]
+    test_data = data[data['image'].isin(test_images)]
+
+    return train_data, val_data, test_data
+
+def move_images(source_dir: str, target_dir: str, image_list: List[str]):
+    """
+    Move the images from the source directory to the target directory.
+
+    Parameters
+    ----------
+    source_dir : str
+        The source directory.
+    target_dir : str
+        The target directory.
+    image_list : List[str]
+        The list of image filenames to move.
+    """
+    if not os.path.exists(target_dir):
+        os.makedirs(target_dir)
+    
+    for img in image_list:
+        shutil.move(os.path.join(source_dir, img), target_dir)
+
+def save_captions(data: pd.DataFrame, filename: str):
+    """
+    Save the captions to a file.
+
+    Parameters
+    ----------
+    data : pd.DataFrame
+        The data containing the captions.
+    filename : str
+        The filename to save the captions to.
+    """
+    data.to_csv(filename, index=False)
 
 if __name__ == '__main__':
     
@@ -307,7 +379,7 @@ if __name__ == '__main__':
     
     # -------------------------- Transformation pipeline ------------------------- #
     
-    transform = transforms.Compose([transforms.Resize((224, 224)), transforms.ToTensor()])
+    transform = transforms.Compose([transforms.Resize((256, 256)), transforms.ToTensor()])
     
     loader, dataset = data_loader(
         image_dir=args.image_dir,
@@ -316,7 +388,8 @@ if __name__ == '__main__':
         batch_size=args.batch_size,
         num_workers=args.num_workers,
         shuffle=args.shuffle,
-        pin_memory=args.pin_memory
+        pin_memory=args.pin_memory,
+        test_mode=args.test_mode
     )
     
     # --------------------- Log information about the dataset -------------------- #
@@ -327,10 +400,23 @@ if __name__ == '__main__':
     num_batches = len(loader)
     logger.info(f'Number of batches: {num_batches}')
     
-    # Print out 5 batches of data
-    for idx, (images, captions) in enumerate(loader):
-        logger.info(f'Batch {idx + 1}/{num_batches}')
-        logger.info(f'Images shape: {images.shape}')
-        logger.info(f'Captions shape: {captions.shape}')
-        if idx == 4:
-            break
+    # -------------------------- Train, val, test split -------------------------- #
+    
+    # Split the data
+    data = pd.read_csv(args.captions_file)
+    train_data, val_data, test_data = split_data(data, val_ratio=0.2)
+    
+    # Create directories for train, validation, and test images
+    train_dir = os.path.join(args.image_dir, 'train')
+    val_dir = os.path.join(args.image_dir, 'val')
+    test_dir = os.path.join(args.image_dir, 'test')
+
+    # Move the images to their respective directories
+    move_images(args.image_dir, train_dir, train_data['image'].unique())
+    move_images(args.image_dir, val_dir, val_data['image'].unique())
+    move_images(args.image_dir, test_dir, test_data['image'].unique())
+
+    # Save the split captions
+    save_captions(train_data, os.path.join(os.path.dirname(args.captions_file), 'train_captions.txt'))
+    save_captions(val_data, os.path.join(os.path.dirname(args.captions_file), 'val_captions.txt'))
+    save_captions(test_data, os.path.join(os.path.dirname(args.captions_file), 'test_captions.txt'))
